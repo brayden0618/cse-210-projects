748853403e5f505241e1da78b472084f756250c0
Brayden Wayman <bwayman@byupathway.edu>
2025-09-02 13:55:45 -0600

    Excercise 3 Done!

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Parts 1 and 2
+        // Console.Write("What is the magic number? ");
+        // int magic = int.Parse(Console.ReadLine());
+
+        // Part 3
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        while (guess != magic)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (magic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("That is Correct!");
+            }
+        }
     }
 }
