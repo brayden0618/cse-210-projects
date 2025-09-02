673c6150a9385c14bc14b3c9d2f0465ad4024fb5
Brayden Wayman <bwayman@byupathway.edu>
2025-09-02 15:14:38 -0600

    Excercise 5 Done!

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -12,27 +12,36 @@ class Program
         int squaredNumber = SquareNumber(userNumber);
         DisplayResult(userName, squaredNumber);
 
+        //Welcome Message
         static void DisplayWelcome()
         {
             Console.WriteLine("Welcome to the program!");
         }
+
+        //Username
         static string PromptUserName()
         {
             Console.Write("Please enter your name: ");
             string name = Console.ReadLine();
             return name;
         }
+
+        //Favorite Number
         static int PromptUserNumber()
         {
             Console.Write("Please enter your favorite number: ");
             int number = int.Parse(Console.ReadLine());
             return number;
         }
+
+        //Squared Number
         static int SquareNumber(int SquareNumber)
         {
             int square = SquareNumber * SquareNumber;
             return square;
         }
+
+        // Results
         static void DisplayResult(string name, int square)
         {
             Console.WriteLine($"{name}, the square of your number is {square}");
